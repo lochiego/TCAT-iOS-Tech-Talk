8644618ef8bd6c3e736cae87c3269dfdb73e96a3
Eric Gonzalez <ericgonzalez@tees.tamus.edu>
2015-06-03 18:00:22 -0500

    Added Note protocol

---
+++ TaskManagement/TaskManagement/Notes.swift
@@ -0,0 +1,15 @@
+//
+//  Notes.swift
+//  TaskManagement
+//
+//  Created by Eric Gonzalez on 6/3/15.
+//  Copyright (c) 2015 Texas A&M Engineering Experiment Station. All rights reserved.
+//
+
+import Foundation
+
+public protocol Note {
+    var name: String { get }
+    var description: String { get }
+}
+}
