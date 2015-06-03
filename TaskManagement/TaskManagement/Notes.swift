11914a44c349cc1b9823e6edf5a7d2a0f364b4e4
Eric Gonzalez <ericgonzalez@tees.tamus.edu>
2015-06-03 18:02:20 -0500

    Defined Reminder struct

--- TaskManagement/TaskManagement/Notes.swift
+++ TaskManagement/TaskManagement/Notes.swift
@@ -12,4 +12,18 @@ public protocol Note {
     var name: String { get }
     var description: String { get }
 }
+
+public struct Reminder: Note {
+    public var name: String
+    public var description: String
+    public var deadline: NSDate
+    public var alarm: NSDate?
+    
+    public init(name: String, description: String, deadline: NSDate, alarm: NSDate? = nil)
+    {
+        self.name = name
+        self.description = description
+        self.deadline = deadline
+        self.alarm = alarm
+    }
 }
