9ff15a71d1ef19af6d4a2b4834687670000dd9ec
Eric Gonzalez <ericgonzalez@tees.tamus.edu>
2015-06-03 17:20:16 -0500

    Created Swift project

---
+++ TaskManagement/TaskManagement/TaskManagement.h
@@ -0,0 +1,19 @@
+//
+//  TaskManagement.h
+//  TaskManagement
+//
+//  Created by Eric Gonzalez on 6/3/15.
+//  Copyright (c) 2015 Texas A&M Engineering Experiment Station. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TaskManagement.
+FOUNDATION_EXPORT double TaskManagementVersionNumber;
+
+//! Project version string for TaskManagement.
+FOUNDATION_EXPORT const unsigned char TaskManagementVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TaskManagement/PublicHeader.h>
+
+
