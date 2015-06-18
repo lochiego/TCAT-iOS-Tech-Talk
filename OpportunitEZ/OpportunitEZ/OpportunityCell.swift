1f9bfcce400df3cfc013aa455bc5b3d29a0bd0ce
Eric Gonzalez <ericgonzalez@tees.tamus.edu>
2015-06-18 17:36:19 -0500

    Added classes

---
+++ OpportunitEZ/OpportunitEZ/OpportunityCell.swift
@@ -0,0 +1,24 @@
+//
+//  OpportunityCell.swift
+//  OpportunitEZ
+//
+//  Created by Eric Gonzalez on 6/19/15.
+//  Copyright (c) 2015 Texas A&M Engineering Experiment Station. All rights reserved.
+//
+
+import UIKit
+
+class OpportunityCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
